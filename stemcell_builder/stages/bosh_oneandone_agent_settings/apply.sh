ade5fea03c2d4df40112ffcfbc6674086bc6c5a1
alibazlamit <ali.bazlamit@hotmail.com>
2018-03-06 20:55:14 +0100

    initial oneandone support

---
+++ stemcell_builder/stages/bosh_oneandone_agent_settings/apply.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+base_dir=$(readlink -nf $(dirname $0)/../..)
+source $base_dir/lib/prelude_apply.bash
+source $base_dir/lib/prelude_agent.bash
+
+cat > $chroot/var/vcap/bosh/agent.json <<JSON
+{
+  "Platform": {
+    "Linux": {
+      "CreatePartitionIfNoEphemeralDisk": true,
+      $(get_partitioner_type_mapping)
+      "DevicePathResolutionType": "virtio",
+      "VirtioDevicePrefix": "google"
+    }
+  },
+  "Infrastructure": {
+    "Settings": {
+      "Sources": [
+        {
+          "Type": "InstanceMetadata",
+          "URI": "http://169.254.169.254",
+          "SettingsPath": "/computeMetadata/v1/instance/attributes/bosh_settings",
+          "Headers": {
+            "Metadata-Flavor": "Google"
+          }
+        }
+      ],
+
+      "UseServerName": true,
+      "UseRegistry": false
+    }
+  }
+}
+JSON
